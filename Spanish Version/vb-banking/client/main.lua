f60cc01f8dd33aa356bb6ec75300223691606847
visibait <33099726+visibait@users.noreply.github.com>
2021-05-17 16:44:02 +0200

    VB-Banking: Fixes

--- Spanish Version/vb-banking/client/main.lua
+++ Spanish Version/vb-banking/client/main.lua
@@ -13,7 +13,7 @@ local atbank = false
 Citizen.CreateThread(function()
 
   while ESX == nil do
-		TriggerEvent('esx:getSantaFeObjectnohack', function(obj) ESX = obj end)
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(0)
 	end
 
