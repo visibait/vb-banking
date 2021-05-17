411c5563ebb2cf542e8e38c2a9bcda1a3c0f1c70
visibait <33099726+visibait@users.noreply.github.com>
2021-05-17 16:58:49 +0200

    VB-Banking: Fixes

--- Spanish Version/vb-banking/server/main.lua
+++ Spanish Version/vb-banking/server/main.lua
@@ -14,16 +14,12 @@ AddEventHandler('vb-banking:server:depositvb', function(amount, inMenu)
 	local _char = ESX.GetPlayerFromId(_src)
 	amount = tonumber(amount)
 	Citizen.Wait(50)
-	if inMenu then
-		if amount == nil or amount <= 0 or amount > _char.getMoney() then
-			TriggerClientEvent('chatMessage', _src, "Cantidad Inválida.")
-		else
-			_char.removeMoney(amount)
-			_char.addAccountMoney('bank', tonumber(amount))
-			_char.showNotification("Has ingresado $"..amount)
-		end
+	if amount == nil or amount <= 0 or amount > _char.getMoney() then
+		TriggerClientEvent('chatMessage', _src, "Cantidad Inválida.")
 	else
-		DropPlayer(_src, "El jugador ha intentado depositar dinero en el banco sin estar cerca de uno.")
+		_char.removeMoney(amount)
+		_char.addAccountMoney('bank', tonumber(amount))
+		_char.showNotification("Has ingresado $"..amount)
 	end
 end)
 
@@ -34,17 +30,13 @@ AddEventHandler('vb-banking:server:withdrawvb', function(amount, inMenu)
 	local _base = 0
 	amount = tonumber(amount)
 	_base = _char.getAccount('bank').money
-	if inMenu then
-		Citizen.Wait(100)
-		if amount == nil or amount <= 0 or amount > _base then
-			TriggerClientEvent('chatMessage', _src, "Cantidad Inválida")
-		else
-			_char.removeAccountMoney('bank', amount)
-			_char.addMoney(amount)
-			_char.showNotification("Has retirado $"..amount)
-		end
+	Citizen.Wait(100)
+	if amount == nil or amount <= 0 or amount > _base then
+		TriggerClientEvent('chatMessage', _src, "Cantidad Inválida")
 	else
-		DropPlayer(_src, "El jugador ha intentado sacar dinero del banco sin estar cerca de uno.")
+		_char.removeAccountMoney('bank', amount)
+		_char.addMoney(amount)
+		_char.showNotification("Has retirado $"..amount)
 	end
 end)
 
@@ -52,12 +44,8 @@ RegisterServerEvent('vb-banking:server:balance')
 AddEventHandler('vb-banking:server:balance', function(inMenu)
 	local _src = source
 	local _char = ESX.GetPlayerFromId(_src)
-	if inMenu then
-		local balance = _char.getAccount('bank').money
-		TriggerClientEvent('vb-banking:client:refreshbalance', _src, balance)
-	else
-		DropPlayer(_src, "El jugador ha intentado llamar a un evento del banco sin estar cerca de uno.")
-	end
+	local balance = _char.getAccount('bank').money
+	TriggerClientEvent('vb-banking:client:refreshbalance', _src, balance)
 end)
 
 RegisterServerEvent('vb-banking:server:transfervb')
@@ -66,25 +54,21 @@ AddEventHandler('vb-banking:server:transfervb', function(to, amountt, inMenu)
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	local zPlayer = ESX.GetPlayerFromId(tonumber(to))
 	local balance = 0
-	if inMenu then
-		if zPlayer ~= nil then
-			balance = xPlayer.getAccount('bank').money
-			if tonumber(_source) == tonumber(to) then
-				TriggerClientEvent('chatMessage', _source, "No te puedes transferir dinero a ti mismo")	
-			else
-				if balance <= 0 or balance < tonumber(amountt) or tonumber(amountt) <= 0 then
-					TriggerClientEvent('chatMessage', _source, "No tienes suficiente dinero en el banco.")
-				else
-					xPlayer.removeAccountMoney('bank', tonumber(amountt))
-					zPlayer.addAccountMoney('bank', tonumber(amountt))
-					zPlayer.showNotification("Te han enviado una transferencia de "..amountt.."€ por parte de la ID: ".._source)
-					xPlayer.showNotification("Has enviado una transferencia de "..amountt.."€ a la ID: "..to)
-				end
-			end
+	if zPlayer ~= nil then
+		balance = xPlayer.getAccount('bank').money
+		if tonumber(_source) == tonumber(to) then
+			TriggerClientEvent('chatMessage', _source, "No te puedes transferir dinero a ti mismo")	
 		else
-			TriggerClientEvent('chatMessage', _source, "That Wallet ID is invalid or doesn't exist")
+			if balance <= 0 or balance < tonumber(amountt) or tonumber(amountt) <= 0 then
+				TriggerClientEvent('chatMessage', _source, "No tienes suficiente dinero en el banco.")
+			else
+				xPlayer.removeAccountMoney('bank', tonumber(amountt))
+				zPlayer.addAccountMoney('bank', tonumber(amountt))
+				zPlayer.showNotification("Te han enviado una transferencia de "..amountt.."€ por parte de la ID: ".._source)
+				xPlayer.showNotification("Has enviado una transferencia de "..amountt.."€ a la ID: "..to)
+			end
 		end
 	else
-		DropPlayer(_source, "El jugador ha intentado sacare transferir dinero del banco sin estar cerca de uno.")
+		TriggerClientEvent('chatMessage', _source, "That Wallet ID is invalid or doesn't exist")
 	end
 end)
